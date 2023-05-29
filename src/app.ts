4f32183f33c46ed6838e0aa71f7bc8eab169294a
abdul_jabbar_dev <abdul.jabbar.dev@gmail.com>
2023-05-30 00:00:16 +0600

    [DONE] Run with Database and core Modules

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors'
 
 const app:Application = express()
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+async function run() {
+    try {
+        console.log("⏳ Database connecting.....")
+       await mongoose.connect(config.db_url as string)
+        console.log("🛢️  Database connection successfully") 
  
+ 
+        app.listen(config.port,()=>console.log(".....................Server is running..............................."))
+    } catch (error) {
+        console.log("⛔ Fatal to connected database⛔ : ",error)
+    }
+}
+run()
