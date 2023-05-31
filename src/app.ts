e7fae5a5b6564d9bb2b2b95665361e175a140fbf
Abdul Jabbar <abduljabber1532002@gmail.com>
2023-05-31 15:14:49 +0600

    [config] husky

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express, { Application, Response, Request } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
-          app.use(cors())
+app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: any, res: Response) => {
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Home')
 })
 
