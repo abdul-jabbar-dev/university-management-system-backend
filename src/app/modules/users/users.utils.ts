54438612151ef2d5a2de1f9f6d18b8df920099f8
Abdul Jabbar <abduljabber1532002@gmail.com>
2023-06-02 22:19:59 +0600

    Improved eslintrc roules

--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -2,7 +2,7 @@ import { getLastUserId } from './users.service'
 
 export const generateUserId = async () => {
   try {
-    const lastId = (await getLastUserId()) || (0).toString().padStart(5, '0') 
+    const lastId = (await getLastUserId()) || (0).toString().padStart(5, '0')
     const incrementalId = (parseInt(lastId) + 1).toString().padStart(5, '0')
     return incrementalId
   } catch (error) {
